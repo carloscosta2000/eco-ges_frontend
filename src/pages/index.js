cbedcf896e7764be01318d2b667c56a9a8b66ddd
Badjarda <mestredua@gmail.com>
2022-12-19 14:02:44 -0500

    verify session token done with minor bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { getAllClients } from '../services/ClientService';
+import { verifyToken } from '../services/SessionService';
+import useToken from '../components/useToken';
 
 function Home() {
+	const { token, setToken } = useToken();
 	const [users, setUsers] = useState(null)
 
 	useEffect(() => {
 		async function fetchData() {
-		  const response = await getAllClients();
-		  console.log(response)
-		  setUsers(response);
+			const response = await verifyToken(token);
+			console.log("Verify Token: " + JSON.stringify(response))
+			if("success" in JSON.stringify(response)){
+				const response_clients = await getAllClients();
+				console.log("Users: " + response_clients)
+				setUsers(response_clients);
+			}
 		}
-		//fetchData();
+		  fetchData();
 	  }, []);
 
 	return (
 		<div>
-		<h1>Welcome to GeeksforGeeks</h1>
+		<h1>{token}</h1>
 		</div>
 	);
 }
