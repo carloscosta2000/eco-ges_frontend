afd852a241c5e3aea6d9cd2ebab5746a096129c6
fiaes <franciscofiaes@gmail.com>
2022-12-21 08:50:12 -0500

    Lets go

--- src/services/DiffieHellmanService.js
+++ src/services/DiffieHellmanService.js
@@ -17,40 +17,62 @@ export async function get_user_information(token){
         
         var res = await fetch(public_key_dir)
         var public_key = await res.text()
+
+        console.log(public_key)
         
         var res = await fetch(server_public_key_dir)
         var server_public_key = await res.text()
 
     
-        const alice = crypto.createDiffieHellman(128);
+        const alice = crypto.createDiffieHellman(258);
         const aliceKey = alice.generateKeys('hex');
     
         const prime = alice.getPrime('hex')
+        console.log(prime)
         const generator = alice.getGenerator('hex')
 
-        const encrypted_payload = await crypto.publicEncrypt(server_public_key, Buffer.from(JSON.stringify({prime: prime, generator: generator, key: aliceKey.toString('hex')})));
+        const encrypted_prime = await crypto.publicEncrypt(server_public_key, Buffer.from(prime));
+        const encrypted_generator = await crypto.publicEncrypt(server_public_key, Buffer.from(generator));
+        const encrypted_key = await crypto.publicEncrypt(server_public_key, Buffer.from(aliceKey));
+
+        //const encrypted_payload = await crypto.publicEncrypt(public_key, Buffer.from(prime));
+        //let str = await crypto.privateDecrypt(private_key, encrypted_payload).toString('utf-8')
+        //console.log(str)
+
         console.log("PUBLIC ENCRYPT");
 
 
-
+        //TODO ver se só se pode assinar um valor
         const sign = crypto.createSign('RSA-SHA256');
-        sign.write(encrypted_payload.toString('utf-8'));
+        sign.write(encrypted_prime);
         sign.end();
         const signature = await sign.sign(private_key);
 
-        // TEST À ASSINATURA:
-        // const verify = crypto.createVerify('RSA-SHA256');
-        // verify.write(encrypted_payload.toString('utf-8'));
-        // verify.end();
-        // console.log(verify.verify(public_key, signature));
+
+        //TEST À ASSINATURA:
+        const verify = crypto.createVerify('RSA-SHA256');
+        verify.write(encrypted_prime);
+        verify.end();
+        console.log(verify.verify(public_key, signature));
+
+        console.log(signature)
 
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ token: token, signature: signature, payload: encrypted_payload})
+            body: JSON.stringify({ 
+                token: token, 
+                signature: signature, 
+                prime: encrypted_prime,
+                generator: encrypted_generator,
+                key: encrypted_key
+            })
         };
 
         const response = await fetch('diffie', requestOptions);
+        console.log(await response.json())
+
+        //await crypto.privateDecrypt(private_key, encrypted_payload).toString('utf-8')
 
         //const aliceSecret = alice.computeSecret(bobKey);
 
@@ -61,12 +83,37 @@ export async function get_user_information(token){
     }
 }
 
-function readFile(file) {
-    return new Promise((resolve, reject) => {
-      // Create file reader
-      let reader = new FileReader()
+const urlDecodeBytes = (encoded) => {
+    let decoded = Buffer.from('')
+    for (let i = 0; i < encoded.length; i++) {
+      if (encoded[i] === '%') {
+        const charBuf = Buffer.from(`${encoded[i + 1]}${encoded[i + 2]}`, 'hex')
+        decoded = Buffer.concat([decoded, charBuf])
+        i += 2
+      } else {
+        const charBuf = Buffer.from(encoded[i])
+        decoded = Buffer.concat([decoded, charBuf])
+      }
+    }
+    return decoded
+}
 
-      // Read file
-      reader.readAsArrayBuffer(file)
-    })
+const urlEncodeBytes = (buf) => {
+    let encoded = ''
+    for (let i = 0; i < buf.length; i++) {
+      const charBuf = Buffer.from('00', 'hex')
+      charBuf.writeUInt8(buf[i])
+      const char = charBuf.toString()
+      // if the character is safe, then just print it, otherwise encode
+      if (isUrlSafe(char)) {
+        encoded += char
+      } else {
+        encoded += `%${charBuf.toString('hex').toUpperCase()}`
+      }
+    }
+    return encoded
   }
+
+const isUrlSafe = (char) => {
+    return /[a-zA-Z0-9\-_~.]+/.test(char)
+}
