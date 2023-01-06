757f7b124daf3fc467799e2ebedc762ebdd77a92
fiaes <franciscofiaes@gmail.com>
2023-01-06 07:36:24 -0500

    Console.logs for screenshots

--- src/services/DiffieHellmanService.js
+++ src/services/DiffieHellmanService.js
@@ -28,8 +28,8 @@ export async function diffie_hellman(token){
     console.log(response)
 
     //TEST The server Certificate
-    const server_x509 = new crypto.X509Certificate(response.certificate);
-    console.log(server_x509)
+    //const server_x509 = new crypto.X509Certificate(response.certificate);
+    //console.log(server_x509)
 
     //TEST À ASSINATURA:
     const verify = crypto.createVerify('RSA-SHA256');
@@ -60,21 +60,20 @@ export async function diffie_hellman(token){
     const bobKey = bob.generateKeys();
     const secret = bob.computeSecret(aliceKey);
 
-    console.log("end-diffie Values to be sent")
-    console.log("\t AlicePrime: " + alicePrime.toString('hex'));
-    console.log("\t AliceGenerator: " + aliceGenerator.toString('hex'));
-    console.log("\n AliceKey: " + aliceKey.toString('hex'));
-    console.log("\t BobKey: " + bobKey.toString('hex'));
     
     const encrypted_alicePrime = crypto.publicEncrypt(server_public_key, alicePrime);
     const encrypted_aliceGenerator = crypto.publicEncrypt(server_public_key, aliceGenerator);
     const encrypted_aliceKey = crypto.publicEncrypt(server_public_key, aliceKey);
     const encrypted_bobKey = crypto.publicEncrypt(server_public_key, bobKey);
 
+    console.log("end-diffie Values to be sent (Encrypted)")
+    console.log("\t Encrypted AlicePrime: " + encrypted_alicePrime.toString('hex'));
+    console.log("\t Encrypted AliceGenerator: " + encrypted_aliceGenerator.toString('hex'));
+    console.log("\n Encrypted AliceKey: " + encrypted_aliceKey.toString('hex'));
+    console.log("\t Encrypted BobKey: " + encrypted_bobKey.toString('hex'));
+    
+
     const sign = crypto.createSign('RSA-SHA256');
-    sign.write(encrypted_alicePrime);
-    sign.write(encrypted_aliceGenerator);
-    sign.write(encrypted_aliceKey);
     sign.write(encrypted_bobKey);
     sign.end();
     const signature = await sign.sign(private_key);
@@ -85,9 +84,6 @@ export async function diffie_hellman(token){
         body: JSON.stringify({ 
             token: token, 
             signature: signature, 
-            encrypted_alicePrime: encrypted_alicePrime,
-            encrypted_aliceGenerator: encrypted_aliceGenerator,
-            encrypted_aliceKey: encrypted_aliceKey,
             encrypted_bobKey: encrypted_bobKey
         })
     };
@@ -104,6 +100,8 @@ export async function diffie_hellman(token){
         telefone: "",
     }
 
+
+
     if(information.morada)
         data_to_send["morada"] = await decypher_data(information.morada, 'hex', 'utf-8', secret)
     if(information.nif)
@@ -115,13 +113,28 @@ export async function diffie_hellman(token){
     if(information.telefone)
         data_to_send["telefone"] = await decypher_data(information.telefone, 'hex', 'utf-8', secret)
 
+    console.log("Critial Data Received Encrypted with Shared Key:");
+    console.log("\t Morada: " + information.morada);
+    console.log("\t Nif: " + information.nif);
+    console.log("\t IBAN: " + information.iban);
+    console.log("\t Email" + information.email);
+    console.log("\t Telefone: " + information.telefone);
+
+    console.log("Critial Data Received Decrypted with Shared Key:");
+    console.log(data_to_send)
+
     return {secret: secret.toString("hex"), information: data_to_send, timestamp: timestamp};
 }
 
 export async function save_data(token, sessionKey, morada, nif, iban, email, telefone){
     
-    console.log("SessionKey");
-    console.log(sessionKey)
+    console.log("Critial Data not Encrypted to be saved: ");
+    console.log("\t Morada: " + morada);
+    console.log("\t Nif: " + nif);
+    console.log("\t IBAN: " + iban);
+    console.log("\t Email" + email);
+    console.log("\t Telefone: " + telefone);
+
 
     let requestOptions = {
         method: 'POST',
@@ -136,15 +149,12 @@ export async function save_data(token, sessionKey, morada, nif, iban, email, tel
         })
     };
 
-
-    const encrypted_morada = await cypher_data(morada, 'utf-8', 'hex', sessionKey);
-    const decrypted_morada = await decypher_data(encrypted_morada, 'hex', 'utf-8', sessionKey);
-    console.log(morada == decrypted_morada)
-    console.log("encrypted_morada")
-    console.log(encrypted_morada)
-    console.log(morada + " " + decrypted_morada)
-
-
+    console.log("Critial Data Encrypted with the shared key, to be saved: ");
+    console.log("\t Morada: " + await cypher_data(morada, 'utf-8', 'hex', sessionKey));
+    console.log("\t Nif: " + await cypher_data(nif, 'utf-8', 'hex', sessionKey));
+    console.log("\t IBAN: " + await cypher_data(iban, 'utf-8', 'hex', sessionKey));
+    console.log("\t Email" + await cypher_data(email, 'utf-8', 'hex', sessionKey));
+    console.log("\t Telefone: " + await cypher_data(telefone, 'utf-8', 'hex', sessionKey));
 
     const response_json = await fetch('save-information', requestOptions);
 }
